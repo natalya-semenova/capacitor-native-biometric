b122dd2cc65784f8184089e403bdf76c03f72d2c
Natalya Semenova <natalya.a.semenova@gmail.com>
2024-03-29 18:00:08 +0100

    update: add dist

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,124 @@
+export declare enum BiometryType {
+    NONE = 0,
+    TOUCH_ID = 1,
+    FACE_ID = 2,
+    FINGERPRINT = 3,
+    FACE_AUTHENTICATION = 4,
+    IRIS_AUTHENTICATION = 5,
+    MULTIPLE = 6
+}
+export interface Credentials {
+    username: string;
+    password: string;
+}
+export interface IsAvailableOptions {
+    /**
+     * Specifies if should fallback to passcode authentication if biometric authentication is not available.
+     */
+    useFallback: boolean;
+}
+export interface AvailableResult {
+    isAvailable: boolean;
+    biometryType: BiometryType;
+    errorCode?: number;
+    isDeviceSecure: boolean;
+}
+export interface BiometricOptions {
+    reason?: string;
+    title?: string;
+    subtitle?: string;
+    description?: string;
+    negativeButtonText?: string;
+    /**
+     * Specifies if should fallback to passcode authentication if biometric authentication fails.
+     */
+    useFallback?: boolean;
+    /**
+     * Only for iOS.
+     * Set the text for the fallback button in the authentication dialog.
+     * If this property is not specified, the default text is set by the system.
+     */
+    fallbackTitle?: string;
+    /**
+     * Only for Android.
+     * Set a maximum number of attempts for biometric authentication. The maximum allowed by android is 5.
+     * @default 1
+     */
+    maxAttempts?: number;
+    /**
+     * Only for Android.
+     */
+    disableConfirmationRequired?: boolean;
+}
+export interface GetCredentialOptions {
+    server: string;
+}
+export interface SetCredentialOptions {
+    username: string;
+    password: string;
+    server: string;
+}
+export interface DeleteCredentialOptions {
+    server: string;
+}
+/**
+ * Keep this in sync with BiometricAuthError in README.md
+ * Update whenever `convertToPluginErrorCode` functions are modified
+ */
+export declare enum BiometricAuthError {
+    UNKNOWN_ERROR = 0,
+    BIOMETRICS_UNAVAILABLE = 1,
+    USER_LOCKOUT = 2,
+    BIOMETRICS_NOT_ENROLLED = 3,
+    USER_TEMPORARY_LOCKOUT = 4,
+    AUTHENTICATION_FAILED = 10,
+    APP_CANCEL = 11,
+    INVALID_CONTEXT = 12,
+    NOT_INTERACTIVE = 13,
+    PASSCODE_NOT_SET = 14,
+    SYSTEM_CANCEL = 15,
+    USER_CANCEL = 16,
+    USER_FALLBACK = 17
+}
+export interface NativeBiometricPlugin {
+    /**
+     * Checks if biometric authentication hardware is available.
+     * @param {IsAvailableOptions} [options]
+     * @returns {Promise<AvailableResult>}
+     * @memberof NativeBiometricPlugin
+     * @since 1.0.0
+     */
+    isAvailable(options?: IsAvailableOptions): Promise<AvailableResult>;
+    /**
+     * Prompts the user to authenticate with biometrics.
+     * @param {BiometricOptions} [options]
+     * @returns {Promise<any>}
+     * @memberof NativeBiometricPlugin
+     * @since 1.0.0
+     */
+    verifyIdentity(options?: BiometricOptions): Promise<void>;
+    /**
+     * Gets the stored credentials for a given server.
+     * @param {GetCredentialOptions} options
+     * @returns {Promise<Credentials>}
+     * @memberof NativeBiometricPlugin
+     * @since 1.0.0
+     */
+    getCredentials(options: GetCredentialOptions): Promise<Credentials>;
+    /**
+     * Stores the given credentials for a given server.
+     * @param {SetCredentialOptions} options
+     * @returns {Promise<any>}
+     * @memberof NativeBiometricPlugin
+     * @since 1.0.0
+     */
+    setCredentials(options: SetCredentialOptions): Promise<void>;
+    /**
+     * Deletes the stored credentials for a given server.
+     * @param {DeleteCredentialOptions} options
+     * @returns {Promise<any>}
+     * @memberof NativeBiometricPlugin
+     * @since 1.0.0
+     */
+    deleteCredentials(options: DeleteCredentialOptions): Promise<void>;
+}
---
+++ dist/plugin.cjs.js
@@ -0,0 +1,76 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@capacitor/core');
+
+exports.BiometryType = void 0;
+(function (BiometryType) {
+    // Android, iOS
+    BiometryType[BiometryType["NONE"] = 0] = "NONE";
+    // iOS
+    BiometryType[BiometryType["TOUCH_ID"] = 1] = "TOUCH_ID";
+    // iOS
+    BiometryType[BiometryType["FACE_ID"] = 2] = "FACE_ID";
+    // Android
+    BiometryType[BiometryType["FINGERPRINT"] = 3] = "FINGERPRINT";
+    // Android
+    BiometryType[BiometryType["FACE_AUTHENTICATION"] = 4] = "FACE_AUTHENTICATION";
+    // Android
+    BiometryType[BiometryType["IRIS_AUTHENTICATION"] = 5] = "IRIS_AUTHENTICATION";
+    // Android
+    BiometryType[BiometryType["MULTIPLE"] = 6] = "MULTIPLE";
+})(exports.BiometryType || (exports.BiometryType = {}));
+/**
+ * Keep this in sync with BiometricAuthError in README.md
+ * Update whenever `convertToPluginErrorCode` functions are modified
+ */
+exports.BiometricAuthError = void 0;
+(function (BiometricAuthError) {
+    BiometricAuthError[BiometricAuthError["UNKNOWN_ERROR"] = 0] = "UNKNOWN_ERROR";
+    BiometricAuthError[BiometricAuthError["BIOMETRICS_UNAVAILABLE"] = 1] = "BIOMETRICS_UNAVAILABLE";
+    BiometricAuthError[BiometricAuthError["USER_LOCKOUT"] = 2] = "USER_LOCKOUT";
+    BiometricAuthError[BiometricAuthError["BIOMETRICS_NOT_ENROLLED"] = 3] = "BIOMETRICS_NOT_ENROLLED";
+    BiometricAuthError[BiometricAuthError["USER_TEMPORARY_LOCKOUT"] = 4] = "USER_TEMPORARY_LOCKOUT";
+    BiometricAuthError[BiometricAuthError["AUTHENTICATION_FAILED"] = 10] = "AUTHENTICATION_FAILED";
+    BiometricAuthError[BiometricAuthError["APP_CANCEL"] = 11] = "APP_CANCEL";
+    BiometricAuthError[BiometricAuthError["INVALID_CONTEXT"] = 12] = "INVALID_CONTEXT";
+    BiometricAuthError[BiometricAuthError["NOT_INTERACTIVE"] = 13] = "NOT_INTERACTIVE";
+    BiometricAuthError[BiometricAuthError["PASSCODE_NOT_SET"] = 14] = "PASSCODE_NOT_SET";
+    BiometricAuthError[BiometricAuthError["SYSTEM_CANCEL"] = 15] = "SYSTEM_CANCEL";
+    BiometricAuthError[BiometricAuthError["USER_CANCEL"] = 16] = "USER_CANCEL";
+    BiometricAuthError[BiometricAuthError["USER_FALLBACK"] = 17] = "USER_FALLBACK";
+})(exports.BiometricAuthError || (exports.BiometricAuthError = {}));
+
+const NativeBiometric = core.registerPlugin("NativeBiometric", {
+    web: () => Promise.resolve().then(function () { return web; }).then((m) => new m.NativeBiometricWeb()),
+});
+
+class NativeBiometricWeb extends core.WebPlugin {
+    constructor() {
+        super();
+    }
+    isAvailable() {
+        throw new Error("Method not implemented.");
+    }
+    verifyIdentity(_options) {
+        throw new Error("Method not implemented.");
+    }
+    getCredentials(_options) {
+        throw new Error("Method not implemented.");
+    }
+    setCredentials(_options) {
+        throw new Error("Method not implemented.");
+    }
+    deleteCredentials(_options) {
+        throw new Error("Method not implemented.");
+    }
+}
+
+var web = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    NativeBiometricWeb: NativeBiometricWeb
+});
+
+exports.NativeBiometric = NativeBiometric;
+//# sourceMappingURL=plugin.cjs.js.map
